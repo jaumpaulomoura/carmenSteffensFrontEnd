274c105ad06981d4d831990f96089caa2b8cccae
João Paulo Moura <jp_moura@carmen.domain>
2024-06-03 09:28:30 -0300

    Styled Automação Bolsa

--- src/pages/AtuBolsa.js
+++ src/pages/AtuBolsa.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Input, Text, Checkbox, CheckboxGroup, Wrap, Box, Button, Flex, useToast, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure } from '@chakra-ui/react';
+import { Select, Input, Text, Checkbox, CheckboxGroup, Wrap, Box, Button, Flex, useToast, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure } from '@chakra-ui/react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import ReactTable from 'react-table-6';
@@ -9,6 +9,7 @@ import axios from 'axios';
 import { ptBR } from 'date-fns/locale';
 import { saveAs } from 'file-saver';
 import * as XLSX from 'xlsx';
+import styled from 'styled-components';
 
 const AtuBolsa = () => {
   const [dataInicial, setDataInicial] = useState(new Date());
@@ -22,10 +23,40 @@ const AtuBolsa = () => {
   const [prestadores, setPrestadores] = useState([]);
   const [prestadoresFiltrados, setPrestadoresFiltrados] = useState([prestadores]);
   const [prestadorSelecionado, setPrestadorSelecionado] = useState('todos');
-
+  const [notNf, setNotNf] = useState('');
+  const [inNf, setInNf] = useState('');
+  const [option, setOption] = useState('');
   const toast = useToast();
 
+  const TableWrapper = styled.div`
+  width: 99.4%;
+  margin-left: 2px;
+  margin: 5px;
+  max-height: 66%; 
+  overflow: auto; 
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  background: #EDF2F7;
+`;
 
+  const handleNotNfChange = (event) => {
+    const value = event.target.value;
+    if (option === 'Apenas') {
+      setInNf(value);
+      setNotNf('');
+    } else if (option === 'Exceto') {
+      setNotNf(value);
+      setInNf('');
+    } else {
+      setNotNf('');
+      setInNf('');
+    }
+  };
+  const handleOptionChange = (event) => {
+    setOption(event.target.value);
+    setNotNf('');
+    setInNf('');
+  };
   const handleViewChange = (view) => {
     setSummaryView(view);
   };
@@ -37,8 +68,10 @@ const AtuBolsa = () => {
       const apiUrlNf = "http://localhost:5000/api/autBolsaNf";
       const paramsNf = {
         data_inicial: formattedDataInicial,
-        data_final: formattedDataFinal
+        data_final: formattedDataFinal,
+        notNf: notNf // Adicionando o valor do input como notNf
       };
+
 
       const responseNf = await axios.get(apiUrlNf, { params: paramsNf });
 
@@ -59,6 +92,7 @@ const AtuBolsa = () => {
   };
 
 
+
   const fetchData = async () => {
     setLoading(true);
     try {
@@ -67,9 +101,14 @@ const AtuBolsa = () => {
       const apiUrl = "http://localhost:5000/api/autBolsa";
       const params = {
         data_inicial: formattedDataInicial,
-        data_final: formattedDataFinal
+        data_final: formattedDataFinal,
+        notNf: notNf
       };
-
+      if (option === 'Apenas') {
+        params.inNf = inNf;
+      } else if (option === 'Exceto') {
+        params.notNf = notNf;
+      }
       const response = await axios.get(apiUrl, { params });
 
       if (response.status === 200) {
@@ -83,16 +122,13 @@ const AtuBolsa = () => {
         }));
 
         setResults(formattedResults);
-
-
-
       }
     } catch (error) {
       console.error("Erro ao buscar dados da API2:", error);
+    } finally {
+      setLoading(false);
     }
-    setLoading(false);
   };
-
 
   const mergeResults = (formattedResultsNf, formattedResults) => {
     const mergedResults = [];
@@ -172,6 +208,33 @@ const AtuBolsa = () => {
       console.error("Erro ao exportar para Excel:", error.message);
     }
   };
+
+
+  const getSummaryPrest = () => {
+    const sortedResults = mergedResults.sort((a, b) => {
+
+      if (a.PRESTADOR < b.PRESTADOR) return -1;
+      if (a.PRESTADOR > b.PRESTADOR) return 1;
+
+
+      return 0;
+    });
+
+    const summary = sortedResults.reduce((acc, item) => {
+      const key = `${item.PRESTADOR}`;
+      if (!acc[key]) {
+        acc[key] = { ...item, QTDE_NF: item.QTDE_FICHAS_FETCHDATANF || 0, QTDE_FICHAS: item.QTDE_FICHAS_FETCHDATA || 0 };
+      } else {
+        acc[key].QTDE_NF += item.QTDE_FICHAS_FETCHDATANF || 0;
+        acc[key].QTDE_FICHAS += item.QTDE_FICHAS_FETCHDATA || 0;
+      }
+      return acc;
+    }, {});
+
+    return Object.values(summary);
+  };
+
+
   const getSummaryDate = () => {
     const sortedResults = mergedResults.sort((a, b) => {
 
@@ -259,6 +322,28 @@ const AtuBolsa = () => {
 
   const renderSummaryTable = () => {
     switch (summaryView) {
+      case 'prest':
+        return (
+
+          <table style={{ marginTop: '10px', width: '100%', borderCollapse: 'collapse' }}>
+            <thead>
+              <tr>
+                <th style={{ border: '1px solid black', padding: '8px' }}>Prestador</th>
+                <th style={{ border: '1px solid black', padding: '8px' }}>Quantidade de Notas</th>
+                <th style={{ border: '1px solid black', padding: '8px' }}>Quantidade de Fichas</th>
+              </tr>
+            </thead>
+            <tbody>
+              {getSummaryPrest().map((item, index) => (
+                <tr key={index}>
+                  <td style={{ border: '1px solid black', padding: '8px' }}>{item.PRESTADOR.split(' ')[0]}</td>
+                  <td style={{ border: '1px solid black', padding: '8px' }}>{item.QTDE_NF}</td>
+                  <td style={{ border: '1px solid black', padding: '8px' }}>{item.QTDE_FICHAS}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        );
       case 'date':
         return (
 
@@ -378,21 +463,22 @@ const AtuBolsa = () => {
         background="#283156"
         borderRadius="10px"
         width="99.4%"
-        h="20vh"
+        maxHeighth="50vh"
         border="1px solid"
         justifyContent="space-between"
         alignItems="center"
         flexDirection="row"
         flexWrap="wrap"
+        pb="5px"
       >
         <Flex direction="column" ml="5px" mt="2px" maxHeight="100vh">
           <Text as="h1" fontSize="xx-large" color="white" mb={4}>
             Automação das Bolsas
           </Text>
           <Flex mr="10px">
-            <Flex direction="column" h="90%" w="40vh">
+            <Flex direction="column" h="90%" w="70%">
               <Flex align="center" mb="10px">
-                <Text w="22%" mr="3px" color="white">
+                <Text w="28%" mr="3px" color="white" fontSize="18px">
                   Data inicial:
                 </Text>
                 <DatePicker
@@ -406,7 +492,7 @@ const AtuBolsa = () => {
                         background: '#EDF2F7',
                         border: '1px solid',
                         borderRadius: '5px',
-                        width: '100%',
+                        width: '120%',
                       }}
                     />
                   }
@@ -414,7 +500,7 @@ const AtuBolsa = () => {
                 />
               </Flex>
               <Flex align="center" mb="10px">
-                <Text w="22%" mr="3px" color="white">
+                <Text w="28%" mr="3px" color="white" fontSize="18px">
                   Data final:
                 </Text>
                 <DatePicker
@@ -428,33 +514,52 @@ const AtuBolsa = () => {
                         background: '#EDF2F7',
                         border: '1px solid',
                         borderRadius: '5px',
-                        width: '100%',
+                        width: '120%',
                       }}
                     />
                   }
                   locale={ptBR}
                 />
               </Flex>
+              <Flex align="center" direction="row">
+                <Select w="28%"  bg="gray.100" placeholder='Todas' onChange={handleOptionChange}>
+                  <option value='Apenas'>Apenas</option>
+                  <option value='Exceto'>Exceto</option>
+                </Select>
+                <Input
+                  bg="gray.100"
+                  w="63%"
+                  ml="5px"
+                  placeholder="NF - Separar por vírgula"
+                  value={option === 'Apenas' ? inNf : notNf}
+                  onChange={handleNotNfChange}
+                  isDisabled={option === ''}
+                />
+
+              </Flex>
             </Flex>
             <Flex
-            mt="12px"
-              ml="-20px"
-              direction="row"
-              justifyItems="flex-start"
+              // mt="2px"
+              // ml="-50px"
+              direction="column"
+              align="flex-start"
               bg="gray.100"
-              h="90%"
-              w="50%"
-              borderRadius="md"
+              h="125px"
+              w="250px"
+              // borderRadius="md"
+              p="10px"
+              boxShadow="md"
+              overflow="auto"
+              border= "1px solid"
+              borderRadius="5px"
             >
               <CheckboxGroup
-                ml="3px"
                 colorScheme="blue"
                 value={prestadorSelecionado}
                 onChange={handlePrestadorChange}
-                h="100%"
                 w="100%"
               >
-                <Wrap ml="3px" h="100%" w="100%" spacing={2}>
+                <Wrap direction="column" spacing="10px" w="100%">
                   <Checkbox value="todos">Todos</Checkbox>
                   {prestadoresUnicos.map((prestador, index) => (
                     <Checkbox key={index} value={prestador.PRESTADOR}>
@@ -464,7 +569,7 @@ const AtuBolsa = () => {
                 </Wrap>
               </CheckboxGroup>
             </Flex>
-            <Input bg="gray.100" w="30%" ml="5px" placeholder="Exceção de Nf" />
+
           </Flex>
         </Flex>
 
@@ -535,80 +640,90 @@ const AtuBolsa = () => {
 
       </Flex >
 
-      <Box>
+      <Flex h="100%">
+
         {loading && <p>Carregando...</p>}
         {!loading && results.length > 0 && (
-          <ReactTable
-            data={prestadoresFiltrados}
-            columns={[
-              { Header: 'COD_PREST', accessor: 'COD_PREST' },
-              { Header: 'PRESTADOR', accessor: 'PRESTADOR' },
-              { Header: 'ANO_FICHA', accessor: 'ANO_FICHA' },
-              { Header: 'FICHAS', accessor: 'FICHAS' },
-              { Header: 'DATA_ENV_FICHA', accessor: 'DATA_ENV_FICHA' },
-              { Header: 'HORA_ENV_FICHA', accessor: 'HORA_ENV_FICHA' },
-              { Header: 'USUARIO_ENV', accessor: 'USUARIO_ENV' },
-              { Header: 'DATA_BX_FICHA', accessor: 'DATA_BX_FICHA' },
-              { Header: 'HORABX_FICHA', accessor: 'HORABX_FICHA' },
-              { Header: 'USUARIO_BX', accessor: 'USUARIO_BX' },
-              { Header: 'PLANO', accessor: 'PLANO' },
-              { Header: 'SECAO', accessor: 'SECAO' },
-              { Header: 'MODELO', accessor: 'MODELO' },
-              { Header: 'QTDE_FICHAS', accessor: 'QTDE_FICHAS' },
-              { Header: 'VLRUNIT', accessor: 'VLRUNIT' },
-              { Header: 'VLRTOTAL', accessor: 'VLRTOTAL' },
-              { Header: 'VLRMATPRIMA', accessor: 'VLRMATPRIMA' },
-              { Header: 'RETENÇÃO', accessor: 'RETENÇÃO' },
-              { Header: 'VLRDEP', accessor: 'VLRDEP' },
-              { Header: 'NF', accessor: 'NF' },
-              { Header: 'STATUS', accessor: 'STATUS' },
-              { Header: 'DTENTREGA', accessor: 'DTENTREGA' },
-              { Header: 'ITEM', accessor: 'ITEM' },
-              { Header: 'QTD_NF', accessor: 'QTD_NF' },
-              { Header: 'CODPRO61', accessor: 'CODPRO61' },
-              { Header: 'MOD4DIG', accessor: 'MOD4DIG' },
-              { Header: 'PEDFORN', accessor: 'PEDFORN' },
-              { Header: 'VLRDESC', accessor: 'VLRDESC' },
-              { Header: 'VLRACRES', accessor: 'VLRACRES' },
-              { Header: 'VLRFRETE', accessor: 'VLRFRETE' },
-              { Header: 'VLROUTRAS', accessor: 'VLROUTRAS' },
-              { Header: 'QTD2', accessor: 'QTD2' },
-              { Header: 'UNIT', accessor: 'UNIT' },
-              { Header: 'OP', accessor: 'OP' },
-              { Header: 'CFOP', accessor: 'CFOP' },
-              { Header: 'SEQ', accessor: 'SEQ' },
-              { Header: 'DESC', accessor: 'DESC' },
-              { Header: 'ACRES', accessor: 'ACRES' },
-              { Header: 'IPI', accessor: 'IPI' },
-              { Header: 'ICMS', accessor: 'ICMS' },
-              { Header: 'DESCITEM', accessor: 'DESCITEM' },
-              { Header: 'CCUSTO', accessor: 'CCUSTO' },
-              { Header: 'GRUPODESP', accessor: 'GRUPODESP' },
-              { Header: 'CODDESP', accessor: 'CODDESP' },
-              { Header: 'VLRICMS', accessor: 'VLRICMS' },
-              { Header: 'COMP', accessor: 'COMP' },
-              { Header: 'PAGTO', accessor: 'PAGTO' },
-              { Header: 'FECHAMENTO', accessor: 'FECHAMENTO' },
-            ]}
-            defaultPageSize={10}
-            className="-striped -highlight"
-            previousText="Anterior"
-            nextText="Próximo"
-            loadingText="Carregando..."
-            noDataText="Nenhum dado encontrado"
-            pageText="Página"
-            ofText="de"
-            rowsText="linhas"
-          />
+          <TableWrapper>
+            <ReactTable
+              data={prestadoresFiltrados}
+              columns={[
+                { Header: 'COD_PREST', accessor: 'COD_PREST' },
+                { Header: 'PRESTADOR', accessor: 'PRESTADOR' },
+                { Header: 'ANO_FICHA', accessor: 'ANO_FICHA' },
+                { Header: 'FICHAS', accessor: 'FICHAS' },
+                { Header: 'DATA_ENV_FICHA', accessor: 'DATA_ENV_FICHA' },
+                { Header: 'HORA_ENV_FICHA', accessor: 'HORA_ENV_FICHA' },
+                { Header: 'USUARIO_ENV', accessor: 'USUARIO_ENV' },
+                { Header: 'DATA_BX_FICHA', accessor: 'DATA_BX_FICHA' },
+                { Header: 'HORABX_FICHA', accessor: 'HORABX_FICHA' },
+                { Header: 'USUARIO_BX', accessor: 'USUARIO_BX' },
+                { Header: 'PLANO', accessor: 'PLANO' },
+                { Header: 'SECAO', accessor: 'SECAO' },
+                { Header: 'MODELO', accessor: 'MODELO' },
+                { Header: 'QTDE_FICHAS', accessor: 'QTDE_FICHAS' },
+                { Header: 'VLRUNIT', accessor: 'VLRUNIT' },
+                { Header: 'VLRTOTAL', accessor: 'VLRTOTAL' },
+                { Header: 'VLRMATPRIMA', accessor: 'VLRMATPRIMA' },
+                { Header: 'RETENÇÃO', accessor: 'RETENÇÃO' },
+                { Header: 'VLRDEP', accessor: 'VLRDEP' },
+                { Header: 'NF', accessor: 'NF' },
+                { Header: 'STATUS', accessor: 'STATUS' },
+                { Header: 'DTENTREGA', accessor: 'DTENTREGA' },
+                { Header: 'ITEM', accessor: 'ITEM' },
+                { Header: 'QTD_NF', accessor: 'QTD_NF' },
+                { Header: 'CODPRO61', accessor: 'CODPRO61' },
+                { Header: 'MOD4DIG', accessor: 'MOD4DIG' },
+                { Header: 'PEDFORN', accessor: 'PEDFORN' },
+                { Header: 'VLRDESC', accessor: 'VLRDESC' },
+                { Header: 'VLRACRES', accessor: 'VLRACRES' },
+                { Header: 'VLRFRETE', accessor: 'VLRFRETE' },
+                { Header: 'VLROUTRAS', accessor: 'VLROUTRAS' },
+                { Header: 'QTD2', accessor: 'QTD2' },
+                { Header: 'UNIT', accessor: 'UNIT' },
+                { Header: 'OP', accessor: 'OP' },
+                { Header: 'CFOP', accessor: 'CFOP' },
+                { Header: 'SEQ', accessor: 'SEQ' },
+                { Header: 'DESC', accessor: 'DESC' },
+                { Header: 'ACRES', accessor: 'ACRES' },
+                { Header: 'IPI', accessor: 'IPI' },
+                { Header: 'ICMS', accessor: 'ICMS' },
+                { Header: 'DESCITEM', accessor: 'DESCITEM' },
+                { Header: 'CCUSTO', accessor: 'CCUSTO' },
+                { Header: 'GRUPODESP', accessor: 'GRUPODESP' },
+                { Header: 'CODDESP', accessor: 'CODDESP' },
+                { Header: 'VLRICMS', accessor: 'VLRICMS' },
+                { Header: 'COMP', accessor: 'COMP' },
+                { Header: 'PAGTO', accessor: 'PAGTO' },
+                { Header: 'FECHAMENTO', accessor: 'FECHAMENTO' },
+              ]}
+              defaultPageSize={13}
+              className="-striped -highlight"
+              previousText="Anterior"
+              nextText="Próximo"
+              loadingText="Carregando..."
+              noDataText="Nenhum dado encontrado"
+              pageText="Página"
+              ofText="de"
+              rowsText="linhas"
+            // style={{
+            //   background: '#EDF2F7',
+            //   border: '1px solid #ddd',
+            //   borderRadius: '5px',
+            //   width: '100%',
+            // }}
+            />
+          </TableWrapper>
         )}
         {!loading && results.length === 0 && <p>Nenhum resultado encontrado.</p>}
-      </Box>
+      </Flex>
       <Modal isOpen={isOpen} onClose={onClose} size="2xl">
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Resumo por Data e Prestador</ModalHeader>
           <ModalCloseButton />
           <ModalBody maxHeight="70vh" overflowY="auto">
+            <Button mr={2} onClick={() => handleViewChange('prest')}>Por Prestador</Button>
             <Button mr={2} onClick={() => handleViewChange('date')}>Por Data</Button>
             <Button mr={2} onClick={() => handleViewChange('nf')}>Por NF</Button>
             <Button onClick={() => handleViewChange('modelo')}>Por Modelo</Button>
